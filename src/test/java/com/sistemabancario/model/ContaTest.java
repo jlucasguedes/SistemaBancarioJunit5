f58e0605785b82a0e84c5d6108c13c3905cf1761
João Lucas Guedes de Oliveira <iftolucas@gmail.com>
2021-06-15 00:04:30 -0300

    #3.2 🏦👨‍🔬 SISTEMA BANCÁRIO COM TDD ☀️ 09/06 🌒 31/05

--- src/test/java/com/sistemabancario/model/ContaTest.java
+++ src/test/java/com/sistemabancario/model/ContaTest.java
@@ -62,4 +62,67 @@ public class ContaTest {
 
     }
 
+    @Test
+    void testeGetSaldoTotal() {
+        final double limite = 500;
+        final double esperado = limite;
+        final Conta instance = new Conta();
+        instance.setEspecial(true);
+        instance.setLimite(limite);
+        final double obtido = instance.getSaldoTotal();
+        assertEquals(esperado, obtido);
+
+    }
+
+    @Test
+    void testeDepositoDinheiro() {
+        final double limite = 500.6, deposito = 500.8, esperado = 1001.4;
+        final Conta instance = new Conta();
+        instance.setEspecial(true);
+        instance.setLimite(limite);
+        instance.depositoDinheiro(deposito);
+
+        final double obtido = instance.getSaldoTotal();
+        assertEquals(esperado, obtido, 0.001);
+    }
+
+    @Test
+    void testeSetDepositoDinheiroTipoMovimentacao() {
+        final char esperado = 'C';
+        final Conta instance = new Conta();
+        instance.depositoDinheiro(500);
+        final int indexUltimaMovimentacao = instance.getMovimentacoes().size() - 1;
+        final char obtido = instance.getMovimentacoes().get(indexUltimaMovimentacao).getTipo();
+        assertEquals(esperado, obtido);
+    }
+
+    @Test
+    void testeSetDepositoDinheiroMovimentacaoConfirmada() {
+        final Conta instance = new Conta();
+        instance.depositoDinheiro(500);
+        final int indexUltimaMovimentacao = instance.getMovimentacoes().size() - 1;
+        assertTrue(instance.getMovimentacoes().get(indexUltimaMovimentacao).isConfirmada());
+    }
+
+    @Test
+    void testeSetDepositoDinheiroMovimentacaoValorArmazenado() {
+        final double valor = 500;
+        final Conta instance = new Conta();
+        instance.depositoDinheiro(valor);
+        final int indexUltimaMovimentacao = instance.getMovimentacoes().size() - 1;
+        final Movimentacao movimentacao = instance.getMovimentacoes().get(indexUltimaMovimentacao);
+        final double obtido = movimentacao.getValor();
+        assertEquals(valor, obtido);
+    }
+
+    @Test
+    void testeSetDepositoDinheiroMovimentacaoIncluida() {
+        final double valor = 500;
+        final Conta instance = new Conta();
+        final int esperado = instance.getMovimentacoes().size() + 1;
+        instance.depositoDinheiro(valor);
+        final int obtido = instance.getMovimentacoes().size();
+        assertEquals(esperado, obtido);
+    }
+
 }
